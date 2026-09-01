906c49ca29ac527603e75d0841fcf42e1e412c77
d141513 <inigo.rezusta.iglesias@navarra.es>
2026-09-01 12:05:24 +0200

    Fix

--- src/app/core/interceptors/auth-token-interceptor.ts
+++ src/app/core/interceptors/auth-token-interceptor.ts
@@ -1,11 +1,11 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
-import { AuthService } from '../../features/auth/services/auth-service';
+//import { AuthService } from '../../features/auth/services/auth-service';
 
 /** Añade el token de sesión a las peticiones dirigidas a la API mock. */
 export const authTokenInterceptor: HttpInterceptorFn = (req, next) => {
-  const authService = inject(AuthService);
-  const token = authService.token();
+  //const authService = inject(AuthService);
+  const token = ''; // authService.token();
 
   if (!token || !req.url.startsWith('/api/')) {
     return next(req);
